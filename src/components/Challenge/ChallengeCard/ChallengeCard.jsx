7210a632d8eced7684b71c92fb72747afa5c12c7
sumforest-ha <harim227@gmail.com>
2026-03-20 18:47:10 +0900

    chor:이미지관련코드 수정

--- src/components/Challenge/ChallengeCard/ChallengeCard.jsx
+++ src/components/Challenge/ChallengeCard/ChallengeCard.jsx
@@ -1,10 +1,16 @@
+'use client'
+import deadlineClock from '@public/img/challengeCard/deadlineClock.svg';
+import clock from '@public/img/challengeCard/clock.svg';
+import fullPerson from '@public/img/challengeCard/fullPerson.svg';
+import person from '@public/img/challengeCard/person.svg';
+import menu from '@public/img/btn/menu.svg';
 import { formatDate } from '@/utils/dateUtils';
 import { Badge } from '../../Common/Badge';
 import { FIELD_MAP, DOCUMENT_TYPE_MAP } from '@/constants/challengeConstants';
 import Image from 'next/image';
-import { IMAGE_SIZES } from '@/constants/uiDimensions';
 import { cardContainer } from './ChallengeCard.css';
 import * as styles from './ChallengeCard.css';
+
 export default function ChallengeCard({ data }) {
   if (!data) return null;
 
@@ -26,36 +32,21 @@ export default function ChallengeCard({ data }) {
         {isClosedExpired ? (
           <Badge badgeStyle="closedStatus" color="closedExpired">
             <div className={styles.badgeInner}>
-              <Image
-                src="/img/challengeCard/deadlineClock.svg"
-                alt="챌린지 마감"
-                width={IMAGE_SIZES.FUll_PERSON}
-                height={IMAGE_SIZES.FUll_PERSON}
-              />
+              <Image src={deadlineClock} alt="챌린지 마감" />
               챌린지가 마감되었어요
             </div>
           </Badge>
         ) : isClosedFull ? (
           <Badge badgeStyle="closedStatus" color="closedFull">
             <div className={styles.badgeInner}>
-              <Image
-                src="/img/challengeCard/fullPerson.svg"
-                alt="모집완료"
-                width={IMAGE_SIZES.FUll_PERSON}
-                height={IMAGE_SIZES.FUll_PERSON}
-              />
+              <Image src={fullPerson} alt="모집완료" />
               모집이 완료된 상태에요
             </div>
           </Badge>
         ) : null}
       </div>
       <button className={styles.menu}>
-        <Image
-          src="/img/btn/menu.svg"
-          alt="메뉴"
-          width={IMAGE_SIZES.MENU}
-          height={IMAGE_SIZES.MENU}
-        />
+        <Image src={menu} alt="메뉴" />
       </button>
       {/* 드롭다운 추가 */}
       <div>
@@ -72,21 +63,11 @@ export default function ChallengeCard({ data }) {
       <div className={styles.divider}></div>
       <div className={styles.currentStatusContainer}>
         <div className={styles.currentStatus}>
-          <Image
-            src="/img/challengeCard/clock.svg"
-            alt="모집완료"
-            width={IMAGE_SIZES.CLOCK}
-            height={IMAGE_SIZES.CLOCK}
-          />
+          <Image src={clock} alt="마감날짜" />
           {formatDate(deadline, 'ko')} 마감
         </div>
         <div className={styles.currentStatus}>
-          <Image
-            src="/img/challengeCard/person.svg"
-            alt="모집완료"
-            width={IMAGE_SIZES.PERSON}
-            height={IMAGE_SIZES.PERSON}
-          />
+          <Image src={person} alt="참여인원" />
           {currentParticipants} / {maxParticipants} 참여중
         </div>
         {/* 도전 계속하기 / 내 작업물 보기 버튼 추가 */}
--- src/constants/uiDimensions.js
+++
@@ -1,7 +0,0 @@
-export const IMAGE_SIZES = Object.freeze({
-  FUll_PERSON: 16,
-  DEADLINE_CLOCK: 16,
-  CLOCK: 24,
-  PERSON: 24,
-  MENU: 24,
-});
