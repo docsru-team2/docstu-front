ffd669d72cde8811b1106fd9354b240962df17af
sumforest-ha <harim227@gmail.com>
2026-04-01 10:25:38 +0900

    feat: emptyState 추가

--- src/components/MyChallenge/MyChallengeApplied/MyChallengeApplied.jsx
+++ src/components/MyChallenge/MyChallengeApplied/MyChallengeApplied.jsx
@@ -6,12 +6,12 @@ import { SearchBar } from '@/components/Common/SearchBar';
 import SimpleDropdown from '@/components/Common/SimpleDropdown/SimpleDropdown';
 import * as styles from './MyChallengeApplied.css';
 import { EmptyState } from '@/components/Common/EmptyState';
+import { useSearchParams } from 'next/navigation';
 
 export default function MyChallengeApplied({ items, totalCount }) {
-  if (!items.length) {
-    return <EmptyState text={'아직 챌린지가 없어요'} />;
-  }
-  
+  const searchParams = useSearchParams();
+  const keyword = searchParams.get('keyword') || '';
+
   const sortItems = [
     { key: 'createedASC', label: '신청 시간 빠른순', value: 'CREATED_ASC' },
     { key: 'createdDESC', label: '신청 시간 느린순', value: 'CREATED_DESC' },
@@ -34,13 +34,22 @@ export default function MyChallengeApplied({ items, totalCount }) {
           <SimpleDropdown items={sortItems} paramName="orderBy" />
         </div>
       </div>
-      {/* 드롭다운 */}
-      <ChallengeTable
-        dataList={items}
-        getHref={(item) => `/myChallenge/applied/${item.id}`}
-      />
+      {items.length === 0 ? (
+        <EmptyState
+          text={
+            keyword
+              ? `'${keyword}'로 검색된 결과가 없어요.`
+              : '아직 챌린지가 없어요'
+          }
+        />
+      ) : (
+        <ChallengeTable
+          dataList={items}
+          getHref={(item) => `/myChallenge/applied/${item.id}`}
+        />
+      )}
       <div className={styles.paginationBarContainer}>
-      <PaginationBar totalCount={totalCount} />
+        <PaginationBar totalCount={totalCount} />
       </div>
     </div>
   );
