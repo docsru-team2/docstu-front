7c5ae58f23a2438b2b2ac5ec15f59cf2108a9186
sumforest-ha <harim227@gmail.com>
2026-04-02 13:02:04 +0900

    feat:피드백아이템 로직구현완료

--- src/components/Feedback/FeedbackItem/FeedbackItem.jsx
+++ src/components/Feedback/FeedbackItem/FeedbackItem.jsx
@@ -2,10 +2,22 @@
 
 import Image from 'next/image';
 import userIcon from '@public/img/header/user.svg';
-import * as styles from './feedbackItem.css';
+import * as styles from './FeedbackItem.css';
+import { formatDate } from '@/utils/dateUtils';
+import SimpleDropdown from '@/components/Common/SimpleDropdown/SimpleDropdown';
+import { usePathname } from 'next/navigation';
 
 export default function FeedbackItem({ data, onDelete }) {
+  const pathname = usePathname();
+  const isAdmin = pathname.includes('/admin');
+
+  if (!data) return null;
   const { user, content, createdAt } = data;
+
+  const menuItems = [
+    { key: 'delete', label: '삭제하기', action: () => onDelete?.(data) },
+  ];
+
   return (
     <div className={styles.feedbackContainer}>
       <div className={styles.userContainer}>
@@ -13,9 +25,22 @@ export default function FeedbackItem({ data, onDelete }) {
 
         <div className={styles.userInfo}>
           <div>{user.nickname}</div>
-          <div className={styles.createdAtFont}>{createdAt}</div>
+          <div className={styles.createdAtFont}>
+            {formatDate(createdAt, 'shortDatetime')}
+          </div>
         </div>
       </div>
+      {isAdmin && (
+        <div
+          className={styles.menu}
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+          }}
+        >
+          <SimpleDropdown items={menuItems} />
+        </div>
+      )}
       <div>{content}</div>
     </div>
   );
